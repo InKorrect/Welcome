d35dcb22089f02ff03ccb9ed001a727bb5beb0e1
gabriel.plique@gmail.com <gabriel.plique@gmail.com>
2023-05-04 21:34:50 +0200

    ITER_2

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -2,9 +2,23 @@ package ilu2;
 
 public class Welcome {
 	public static String welcome(String input) {
+		String reponse = null;
+		if(" ".equals(input)||input==null||"".equals(input)) {
+			reponse=vide(input);
+		}else {
+			reponse=maj1(input);
+		}
+		return reponse;
+		
+	}
+	private static String maj1(String input) {
 		input = input.substring(0, 1).toUpperCase() + input.substring(1);
 		StringBuilder str = new StringBuilder("Hello, ");
 		str.append(input);
 		return str.toString();
+	}
+	private static String vide(String input) {
+		StringBuilder str2 = new StringBuilder("Hello, my friend");
+		return str2.toString();
 	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -9,6 +9,16 @@ class WelcomeTest {
 	@Test
 	void test1() {
 		assertEquals(Welcome.welcome("bob"),"Hello, Bob");
+		assertEquals(Welcome.welcome("Bob"),"Hello, Bob");
+		assertEquals(Welcome.welcome("Paul"),"Hello, Paul");
+		assertEquals(Welcome.welcome("paul"),"Hello, Paul");
 	}
-
+	
+	@Test
+	void test2() {
+		String verif="Hello, my friend";
+		assertEquals(Welcome.welcome(" "),verif);
+		assertEquals(Welcome.welcome(""),verif);
+		assertEquals(Welcome.welcome(null),verif);
+	}
 }
