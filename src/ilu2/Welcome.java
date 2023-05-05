0090938eabe2d9a4e35f50938b2077a92fc1aafb
gabriel.plique@gmail.com <gabriel.plique@gmail.com>
2023-05-05 10:57:59 +0200

    ITER_7

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -24,17 +24,52 @@ public class Welcome {
 		return str.toString();
 	}
 	
+	private static String nomS(String input,String[] part) {
+		int[] nomMaj=new int[part.length]; int o=0; int nbNomMaj=0;
+		int[] nomMin=new int[part.length]; int m=0; int nbNomMin=0;
+		StringBuilder str4=new StringBuilder("");
+		for(int i=0;i<part.length;i++) {
+			if(!part[i].equals(part[i].toUpperCase())) {
+				nomMin[m]=i;
+				nbNomMin++;m++;
+			}else {
+				nomMaj[o]=i;
+				nbNomMaj++;o++;
+			}
+		}
+		if(nbNomMin>0)str4.append(nomMin(nomMin,part,nbNomMin));
+		if(nbNomMaj>0)str4.append(nomMaj(nomMaj,part,nbNomMaj));
+		return str4.toString();
+	}
+	private static String nomMin(int[] nomMin,String part[],int nbNomMin) {
+		int i;
+		StringBuilder str = new StringBuilder("Hello");
+		for(i=0;i<nbNomMin-1;i++) {
+			str.append(", ");
+			str.append(part[nomMin[i]].substring(0, 1).toUpperCase()+part[nomMin[i]].substring(1));
+		}
+		if(nbNomMin>1)str.append(" and ");
+		else str.append(", ");
+		str.append(part[nomMin[i]].substring(0, 1).toUpperCase()+part[nomMin[i]].substring(1));
+		return str.toString();
+		}
+	
+	private static String nomMaj(int[] nomMaj, String[] part,int nbNomMaj) {
+		int i;
+		StringBuilder str = new StringBuilder(". AND HELLO");
+		for(i=0;i<nbNomMaj-1;i++) {
+			str.append(", ");
+			str.append(part[nomMaj[i]]);
+		}
+		if(nbNomMaj>1)str.append(" AND ");
+		else str.append(", ");
+		str.append(part[nomMaj[i]]);
+		str.append(" !");
+		return str.toString();
+	}
+	
 	private static String vide(String input) {
 		StringBuilder str2 = new StringBuilder("Hello, my friend");
 		return str2.toString();
-	}
-	
-	private static String nomS(String input,String[] part) {
-		StringBuilder str4 = new StringBuilder("Hello");
-		for(int i=0;i<part.length;i++) {
-			String part1=part[i].substring(0, 1).toUpperCase() + part[i].substring(1);
-			str4.append(", ");
-			str4.append(part1);
-		}return str4.toString();
 	}
 }
